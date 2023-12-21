b067622b900ca134b36ff09cb0aab4cd6b5dbbeb
Usama Derbashi <derbashi@tuta.io>
2023-12-21 11:10:04 +0300

    added input box for create modal

--- src/components/ModalCreate.vue
+++ src/components/ModalCreate.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1 class="text-xl">Create Game:</h1>
+    <input
+      type="text"
+      v-model="gameName"
+      placeholder="Game Name"
+      class="py-2 px-1 w-full bg-transparent border-b focus:border-tennis-primary focus:outline-none focus:shadow-[0px_1px_0_0_tennis-secondary]"
+    />
     <button
       @click="$emit('close')"
       class="mt-8 mx-1 py-2 px-6 text-white bg-tennis-primary hover:bg-tennis-secondary"
@@ -15,3 +21,9 @@
     </button>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const gameName = ref("");
+</script>
